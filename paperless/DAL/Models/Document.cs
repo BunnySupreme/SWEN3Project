d01b8935cdaf9878f9409586467e9e6cd5fadb84
Disco-Pickle <s.sacher102@gmail.com>
2025-10-09 15:01:59 +0200

    Document model created

---
+++ paperless/DAL/Models/Document.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace paperless.DAL.Models
+{
+    public class Document
+    {
+        #region Constructors
+        public Document(string title, string content, string summary, List<string> tags)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Content = content;
+            Summary = summary;
+            Tags = string.Join(",", tags);
+            CreationDate = DateTime.UtcNow;
+        }
+        #endregion
+
+        #region Properties
+        [Key][Column("id")]
+        public Guid Id { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("content")]
+        public string Content { get; set; }
+        [Column("summary")]
+        public string Summary { get; set; }
+        [Column("tags")]
+        public string Tags { get; set; }
+        [Column("creationdate")]
+        public DateTime CreationDate { get; set; }
+        #endregion
+    }
+}
