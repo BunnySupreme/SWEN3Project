a2b0002ed37beddcbcfd57e65145ebf9d4b7d1e2
Disco-Pickle <s.sacher102@gmail.com>
2025-10-09 15:32:09 +0200

    Configuration class created

---
+++ paperless/DAL/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace paperless.DAL
+{
+    public class Configuration
+    {
+        #region Configuration
+        private const string _FILENAME = @"C:\Dev\configs\SWEN3Project\swen3project.config";
+        #endregion
+
+        #region Properties
+        public string ConnectionString { get; set; } = String.Empty;
+        #endregion
+
+        #region Instance
+        private static Configuration? _instance = null;
+        public static Configuration Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    if (File.Exists(_FILENAME))
+                    {
+                        _instance = JsonSerializer.Deserialize<Configuration>(File.ReadAllText(_FILENAME));
+                    }
+                    if (_instance == null)
+                    { 
+                        _instance = new Configuration();
+                    }
+                }
+                return _instance;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void Save()
+        {
+            File.WriteAllText(_FILENAME, JsonSerializer.Serialize(this));
+        }   
+        #endregion
+    }
+}
