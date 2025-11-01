1a82849739fc3b39113944b629f312721a37f39f
Disco-Pickle <s.sacher102@gmail.com>
2025-11-01 17:11:20 +0100

    14 - Basic DTO validation setup

---
+++ paperless/src/paperless/Api/Validators/DocumentCreateDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Paperless.Api;
+using Paperless.DAL.Repositories;
+
+namespace paperless.Api.Validators
+{
+    public class DocumentCreateDtoValidator : AbstractValidator<DocumentCreateDto>
+    {
+        #region Fields
+        private readonly IDocumentRepository _repo;
+        #endregion
+
+        #region Constructors
+        public DocumentCreateDtoValidator(IDocumentRepository repo)
+        {
+            _repo = repo;
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required")
+                .MaximumLength(255).WithMessage("Title must not exceed 255 characters");
+
+            // WIP: Add additional rules
+        }
+        #endregion
+    }
+}
---
+++ paperless/src/paperless/Api/Validators/DocumentUpdateDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Paperless.Api;
+using Paperless.DAL.Repositories;
+
+namespace paperless.Api.Validators
+{
+    public class DocumentUpdateDtoValidator : AbstractValidator<DocumentUpdateDto>
+    {
+        #region Fields
+        private readonly IDocumentRepository _repo;
+        #endregion
+
+        #region Constructors
+        public DocumentUpdateDtoValidator(IDocumentRepository repo)
+        {
+            _repo = repo;
+
+            // Don't forget rule for ID here
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required")
+                .MaximumLength(255).WithMessage("Title must not exceed 255 characters");
+
+            // WIP: Add additional rules
+        }
+        #endregion
+    }
+}
